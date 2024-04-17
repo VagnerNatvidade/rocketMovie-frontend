50bc9492c867c8fc9ad4b775eb5c43d5bea65c7d
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-04-16 23:05:22 -0300

    criando hook de autenticação do usuário

--- film-api/src/main.jsx
+++ film-api/src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+
 import { Routes } from "./routes/";
+import { AuthProvider } from "./hooks/auth";
+
+import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/global";
 
@@ -9,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
